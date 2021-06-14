afda5d6e351bcc654028e41a3dd2bfe901d7deb1
Sumit Das <sumitdas9234@gmail.com>
2021-06-14 21:46:40 +0530

    Added Static Files and Views using Webpack | Sumit Das

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
-  entry: path.resolve(__dirname, "./index.js"),
+  entry: path.resolve(__dirname, "index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "api.bundle.js",
   },
   target: "node",
+  plugins: [
+    new CopyWebpackPlugin([
+        {from: path.resolve(__dirname, "public/"),to: path.resolve(__dirname, "dist/public/")} 
+    ]), 
+    new CopyWebpackPlugin([
+      {from: path.resolve(__dirname, "views/"),to: path.resolve(__dirname, "dist/views/")} 
+  ]), 
+]
 };
